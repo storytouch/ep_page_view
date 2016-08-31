10f1104152ff87f483fc15fc6be6ea66ce030fb1
Luiza Pagliari <lpagliari@gmail.com>
2016-08-31 12:18:39 -0300

    Improve organization of CSS
    Extract a CSS file only for styles related to pagination/page breaks.

--- static/js/index.js
+++ static/js/index.js
@@ -142,5 +142,5 @@ function getParam(sname) {
 }
 
 exports.aceEditorCSS = function(hook_name, cb) {
-  return ["/ep_script_page_view/static/css/iframe.css"];
+  return ["/ep_script_page_view/static/css/iframe.css", "/ep_script_page_view/static/css/pagination.css"];
 }
