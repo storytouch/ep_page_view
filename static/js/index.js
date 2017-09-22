142af5dbcb92f00ec77fb34b686eede3670ce479
Luiza Pagliari <lpagliari@gmail.com>
2017-09-22 19:00:49 -0300

    [perf] Disable pagination styles to avoid impact on pasting
    Pagination is not enabled for now, but its styles are impacting the
    pasting of large contents (ex: City of God). Need to re-enable and
    optimize these styles when pagination is available again.
    
    Part of the fixes for https://trello.com/c/3cBqMCx8/1082.

--- static/js/index.js
+++ static/js/index.js
@@ -145,5 +145,7 @@ function getParam(sname) {
 }
 
 exports.aceEditorCSS = function(hook_name, cb) {
-  return ["/ep_script_page_view/static/css/iframe.css", "/ep_script_page_view/static/css/pagination.css"];
+  // TODO make sure pagination.css is optimized for pasting of large contents before re-enabling it
+  return ["/ep_script_page_view/static/css/iframe.css"];
+  // return ["/ep_script_page_view/static/css/iframe.css", "/ep_script_page_view/static/css/pagination.css"];
 }
