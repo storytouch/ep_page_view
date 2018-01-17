9a445903cf6e794af8115593a999de4e43cb508b
Luiza Pagliari <lpagliari@gmail.com>
2018-01-17 12:12:14 -0200

    [perf] Add marker class to lines split to improve performance
    Simplify CSS selector, so paste of large scripts won't take too long to
    complete or make Etherpad be "hanged" for some minutes.
    
    Also simplify other CSS selectors for the same reason.
    
    Fix part of https://trello.com/c/3cBqMCx8/1082.

--- static/js/cssOptimization.js
+++ static/js/cssOptimization.js
@@ -7,31 +7,37 @@ var detailedLinesChangedListener = require('ep_script_scene_marks/static/js/deta
 var BEFORE_PAGE_BREAK_CLASS = 'beforePageBreak';
 var AFTER_PAGE_BREAK_CLASS = 'afterPageBreak';
 
+var FIRST_HALF_CLASS = 'firstHalf';
+var SECOND_HALF_CLASS = 'secondHalf';
+
 exports.init = function() {
-  initializePaginationClass();
-  updatePaginationClassWhenLinesWithPageBreaksAreChanged();
+  initializePaginationClass(BEFORE_PAGE_BREAK_CLASS, AFTER_PAGE_BREAK_CLASS);
+  initializePaginationClass(FIRST_HALF_CLASS, SECOND_HALF_CLASS);
+
+  updatePaginationClassWhenLinesWithPageBreaksAreChanged(BEFORE_PAGE_BREAK_CLASS, AFTER_PAGE_BREAK_CLASS);
+  updatePaginationClassWhenLinesWithPageBreaksAreChanged(FIRST_HALF_CLASS, SECOND_HALF_CLASS);
 }
 
-var initializePaginationClass = function() {
-  var $allLinesWithPageBreaks = utils.getPadInner().find('.' + BEFORE_PAGE_BREAK_CLASS);
-  updatePageBreakClass($allLinesWithPageBreaks.toArray());
+var initializePaginationClass = function(sourceClass, targetClass) {
+  var $allLinesWithPageBreaks = utils.getPadInner().find('.' + sourceClass);
+  updatePageBreakClass($allLinesWithPageBreaks.toArray(), targetClass);
 }
 
-var updatePaginationClassWhenLinesWithPageBreaksAreChanged = function() {
+var updatePaginationClassWhenLinesWithPageBreaksAreChanged = function(sourceClass, targetClass) {
   detailedLinesChangedListener.onLinesAddedOrRemoved(function(linesChanged) {
-    var linesWithPageBreaks = getLinesWithPageBreaks(linesChanged.linesAdded);
-    updatePageBreakClass(linesWithPageBreaks);
+    var linesWithPageBreaks = getLinesWithPageBreaks(linesChanged.linesAdded, sourceClass);
+    updatePageBreakClass(linesWithPageBreaks, targetClass);
   });
 }
 
-var getLinesWithPageBreaks = function(linesChanged) {
+var getLinesWithPageBreaks = function(linesChanged, sourceClass) {
   return _(linesChanged).filter(function(line) {
-    return _(line.classList).contains(BEFORE_PAGE_BREAK_CLASS);
+    return _(line.classList).contains(sourceClass);
   });
 }
 
-var updatePageBreakClass = function(linesWithPageBreaks) {
+var updatePageBreakClass = function(linesWithPageBreaks, targetClass) {
   _(linesWithPageBreaks).each(function(lineWithPageBreak) {
-    $(lineWithPageBreak).next().addClass(AFTER_PAGE_BREAK_CLASS);
+    $(lineWithPageBreak).next().addClass(targetClass);
   });
 }
