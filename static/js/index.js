ab60961d37253ce11c3b0c4ef4d3f8ff704b11a0
Luiza Pagliari <lpagliari@gmail.com>
2019-02-01 12:14:43 -0200

    [refactor] Bundle CSS file
    Extract aceEditorCSS into a separated file, so this plugin can have its
    CSS files bundled.
    
    Implement part of https://trello.com/c/lfLjqYD4/1672.

--- static/js/index.js
+++ static/js/index.js
@@ -140,7 +140,3 @@ function getParam(sname) {
   }
   return sval;
 }
-
-exports.aceEditorCSS = function(hook_name, cb) {
-  return ["/ep_script_page_view/static/css/iframe.css", "/ep_script_page_view/static/css/pagination.css"];
-}
